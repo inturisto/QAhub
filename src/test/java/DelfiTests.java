dbdeb5e6fd2192f288d72194cc29b7a99a76d3ac
inturisto <kiril93@inbox.lv>
2020-06-28 14:54:42 +0300

    New

--- src/test/java/DelfiTests.java
+++ src/test/java/DelfiTests.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DelfiTests {
+    @Test
+    public void firstDelfiTest(){
+        System.setProperty("webdriver.chrome.driver","C://chromedriver.exe");
+        WebDriver driver =new ChromeDriver();
+        driver.get("https://rus.delfi.lv");
+        driver.manage().window().maximize();
+    }
 }
