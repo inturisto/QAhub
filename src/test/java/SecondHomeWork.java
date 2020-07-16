28eab0b7affc517c82f8bbc65557de9dae06fba7
inturisto <kiril93@inbox.lv>
2020-07-16 22:46:30 +0300

    New

--- src/test/java/SecondHomeWork.java
+++ src/test/java/SecondHomeWork.java
@@ -2,9 +2,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SecondHomeWork {
+    private final String WEB_PAGE = "http://rus.delfi.lv";
+    private final String TEXT_TO_COMPARE = "Не обязан отвечать клеветникам: президент Латвии рассказал о " +
+            "своем гражданстве, образовании и родителях  ";
+
     @Test
     public void toCompareHeading() {
         String articleTitleToCheck = "Удар коронавируса: за первое полугодие VID собрал на 1,5% меньше налогов, чем " +
@@ -12,7 +20,7 @@ public class SecondHomeWork {
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://rus.delfi.lv");
+        driver.get(WEB_PAGE);
         String element = driver.findElement(By.xpath(".//h1[contains(@class,'headline__title')]")).getText();
         Assertions.assertEquals(articleTitleToCheck, element, "Does not match!");
         driver.quit();
@@ -24,22 +32,40 @@ public class SecondHomeWork {
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://rus.delfi.lv");
+        driver.get(WEB_PAGE);
         String actualCommentCount = driver.findElement(By.xpath(".//a[@class='comment-count text-red-ribbon']"))
                 .getText().replaceAll("\\p{P}", "");
         Assertions.assertEquals(setCommentCount, actualCommentCount, "Does not match!");
         driver.quit();
     }
-}
+
+    @Test
+    public void toGetArticleNumber() {
+        System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(WEB_PAGE);
+        List<WebElement> elements = driver.findElements(By.xpath(".//h1[contains(@class,'headline__title')]"));
+        System.out.println("Size =" + elements.size());
+        List<String> elementText = new ArrayList<String>();
+        for (WebElement text : elements) {
+            elementText.add(text.getText());
+
+        }
+        for (String outputText : elementText) {
+            System.out.println(outputText);
+
+        }
+
+//        for (int i = 0; i < elementText.size(); i++) {
+//            if (!elementText.get(i+1).equals(TEXT_TO_COMPARE)) {
+//                i++;
+//            } else {
+//                System.out.println(elementText.get(i));
+//                break;
+//            }
 //
-//    @Test
-//    public void delfiTest(){
-//        System.setProperty("webdriver.chrome.driver","C://chromedriver.exe");
-//        WebDriver driver =new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.get("https://rus.delfi.lv");
-//        List<String> articles = new ArrayList<String>();
-//        Integer text = driver.findElement(By.xpath(".//h1")).hashCode();
-//        System.out.println(text);
-//    }
-//}
+//        }
+        driver.quit();
+    }
+}
